bc502fd862351a80d5a7ef8d911a60891e2ddc97
Derick Turner <derick@e-learndesign.co.uk>
2025-04-13 22:04:58 +0100

    IOMAD: 4.5.4 release

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.5.3 (Build: 20250317)'; // Human-friendly version name
+$plugin->release  = '4.5.4 (Build: 20250414)'; // Human-friendly version name
 $plugin->version  = 2025021000;   // The (date) version of this plugin.
 $plugin->requires = 2024100700;   // Requires this Moodle version.
 $plugin->component  = 'block_iomad_company_admin';
